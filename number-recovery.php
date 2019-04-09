0d85dd33792ff2f6de6451ebf8da11b0ec1f4e15
nata <korovina-nataliya@mail.ru>
2019-04-09 12:46:32 +0300

    improvement

--- number-recovery.php
+++ number-recovery.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once __DIR__ . 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv = \Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 $urlCrm = getenv('URL_CRM');
 $apiKey = getenv('API_KEY');
