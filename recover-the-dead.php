986e10270cd60909b467112acb5ae8d377a19add
nata <korovina-nataliya@mail.ru>
2019-04-23 12:44:29 +0300

    edit recover-the-dead.php

--- recover-the-dead.php
+++ recover-the-dead.php
@@ -13,7 +13,7 @@ $client = new \RetailCrm\ApiClient(
     \RetailCrm\ApiClient::V5
 );
 
-$id = 1109;
+$id = 1106;
 
 try {
     $response = $client->request->ordersHistory(['orderId' => $id], null, 20);
@@ -23,7 +23,7 @@ try {
 
 if ($response->isSuccessful()) {
     $totalPageCount = $response->pagination['totalPageCount'];
-    $order = $response->history[0];
+    $order = $response->history[0]['order'];
     $ordersList = [];
     $count = 0;
     for ($page = 1; $page <= $totalPageCount; $page++) {
@@ -32,6 +32,7 @@ if ($response->isSuccessful()) {
             $historyList[] = $history;
         }
     }
+    print_r(['order' => $order]);
     $responseOrdersCreate = $client->request->ordersCreate($order);
     file_put_contents('history.log', json_encode(['DATE' => date('Y-m-d H:i:s'), 'history' => $historyList], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT), FILE_APPEND);
     unset($historyList[0]);
@@ -46,10 +47,11 @@ if ($response->isSuccessful()) {
         $mapping[] = $child;
     }
 
-    foreach ($historyList as $history) {
-        foreach ($mapping as $field){
+    foreach ($mapping as $field){
+        foreach ($historyList as $history) {
             if ($history['field'] == $field->attributes()['id']){
-                $responseOrdersEdit = $client->request->ordersEdit([$field[0] => $history['newValue'], 'id' => $id], 'id');
+                $responseOrdersEdit = $client->request->ordersEdit([$field->__toString() => $history['newValue'], 'id' => $id], 'id');
+                file_put_contents('response.log', json_encode(['DATE' => date('Y-m-d H:i:s'), 'fieldHistory' => $history['field'], 'fieldAttributes' => $field->attributes()['id'], 'newValue' => $history['newValue'], 'fieldCRM' => $field->__toString()], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT), FILE_APPEND);
             }
         }
     }
